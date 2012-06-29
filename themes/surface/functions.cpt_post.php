39721623c606fa97763c6289a7a7323c29141d10
Trevor <trovster@gmail.com>
2012-06-29 13:15:34 +0100

    Post class added

---
+++ themes/surface/functions.cpt_post.php
@@ -0,0 +1,44 @@
+<?php require_once dirname(__FILE__) . '/functions.cpt.php';
+
+class Surface_CPT_Post extends Surface_CTP {
+	
+     protected $_post_type = 'post';
+
+	/**
+	* __construct()
+	* @param	array	$options
+	* return	$this
+	*/
+	public function __construct($options = array()) {
+		parent::__construct($options);
+	
+		return $this;
+	}
+
+	/**
+	* has_link_href
+	* @desc	
+	* @return	boolean
+	*/
+	public function has_link_href() {
+		return true;
+	}
+
+	/**
+	* get_link_href
+	* @desc	
+	* @param	mixed	$default
+	* @return	mixed
+	*/
+	public function get_link_href($default = false) {
+		return get_permalink($this->ID);
+	}
+}
+
+/**
+ * Hook in to WordPress
+ */
+if(class_exists('Surface_CPT_Post')) {
+	$cpt_post = new Surface_CPT_Post('setup');
+	add_action(__FILE__, array(&$cpt_post, 'init'));
+}
