d37b4299dda7ad9266b93c23db5a81fbe6295d71
Trevor <trovster@gmail.com>
2012-08-27 13:18:24 +0100

    Change $page to $the_page, because $page is a WordPress global.

--- themes/surface/page-two-column.php
+++ themes/surface/page-two-column.php
@@ -9,19 +9,19 @@
 	
 	<?php while(have_posts()): the_post(); ?>
 	
-	<?php $page	= Surface_CPT_Page::find_by_id($post->ID); ?>
+	<?php $the_page	= Surface_CPT_Page::find_by_id($post->ID); ?>
 	
 	<div class="hentry">
 		
-		<h1 class="entry-title"><?php $page->the_title(); ?></h1>
+		<h1 class="entry-title"><?php $the_page->the_title(); ?></h1>
 		
 		<div class="entry-content column column-primary">
-			<?php $page->the_content(); ?>
+			<?php $the_page->the_content(); ?>
 		</div>
 		
-		<?php if(!empty($page->has_custom_value('secondary_column'))): ?>
+		<?php if(!empty($the_page->has_custom_value('secondary_column'))): ?>
 		<div class="column column-secondary">
-			<?php echo apply_filters('the_content', $page->custom_value('secondary_column')); ?>
+			<?php echo apply_filters('the_content', $the_page->custom_value('secondary_column')); ?>
 		<!-- end of div .column-secondary -->
 		</div>
 		<?php endif; ?>
--- themes/surface/page.php
+++ themes/surface/page.php
@@ -4,14 +4,14 @@
 	
 	<?php while(have_posts()): the_post(); ?>
 	
-	<?php $page	= Surface_CPT_Page::find_by_id($post->ID); ?>
+	<?php $the_page	= Surface_CPT_Page::find_by_id($post->ID); ?>
 	
 	<div class="hentry">
 		
-		<h1 class="entry-title"><?php $page->the_title(); ?></h1>
+		<h1 class="entry-title"><?php $the_page->the_title(); ?></h1>
 		
 		<div class="entry-content">
-			<?php $page->the_content(); ?>
+			<?php $the_page->the_content(); ?>
 		</div>
 		
 	</div>
