a479a34b0eb62ce848bfcbb220c3f3c4a2ad28ab
Trevor <trovster@gmail.com>
2013-03-04 09:55:08 +0000

    Added is_modern check to app, from http://www.thecssdiv.co.uk/2013/03/on-responsive-design/

--- themes/surface/js/app/app.js
+++ themes/surface/js/app/app.js
@@ -11,6 +11,7 @@ jQuery.noConflict();
 var App = (function (App, $, undefined) {
 
 	App	= {
+		modern:	(querySelector in document && localStorage in window && addEventListener in window),
 		util:	{
 			route:		{},
 			debug:		{
