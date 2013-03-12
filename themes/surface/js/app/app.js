70681b831fe8f7c0b0fe2595dc38fe53477c28a6
Trevor <trovster@gmail.com>
2013-03-12 14:19:22 +0000

    Comment out is_modern

--- themes/surface/js/app/app.js
+++ themes/surface/js/app/app.js
@@ -11,7 +11,7 @@ jQuery.noConflict();
 var App = (function (App, $, undefined) {
 
 	App	= {
-		modern:	(querySelector in document && localStorage in window && addEventListener in window),
+//		is_modern:	(querySelector in document && localStorage in window && addEventListener in window),
 		util:	{
 			route:		{},
 			debug:		{
